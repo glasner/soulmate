57c1d384dc315c6e43a1e58362826508112edb71
Jordan Glasner <jordan@digitalignition.com>
2012-04-01 15:32:56 -0400

    final cleanup

--- lib/soulmate/matcher.rb
+++ lib/soulmate/matcher.rb
@@ -9,37 +9,33 @@ module Soulmate
       words = normalize(term).split(' ').reject do |w|
         w.size < MIN_COMPLETE or Soulmate.stop_words.include?(w)
       end.sort
-      
-      puts "Check for #{words.inspect}"
 
       return [] if words.empty?
-      
-      puts 'Past words.empty'
 
       cachekey = "#{cachebase}:#{words.join('|')}"
 
-      if !options[:cache] || !redis.exists(cachekey)
-        puts "Writing to cache"
-        interkeys = words.map { |word| key "#{base}:#{word}", redis  }
-        puts interkeys
-        redis.zinterstore(key(cachekey,redis), interkeys)
-        redis.expire(cachekey, 10 * 60) # expire after 10 minutes
-      end
-
+      cache(cachekey, words, redis) if !options[:cache] || !redis.exists(cachekey)
+      
       ids = redis.zrevrange(cachekey, 0, options[:limit] - 1)
-      if ids.size > 0
-        results = redis.hmget(database, *ids)
-        results = results.reject{ |r| r.nil? } # handle cached results for ids which have since been deleted
-        results.map { |r| MultiJson.decode(r) }
-      else
-        puts "no ids found #{ids.inspect}"
-        []
-      end
+      return [] if ids.empty?
+      results = redis.hmget(database, *ids)
+      results = results.reject{ |r| r.nil? } # handle cached results for ids which have since been deleted
+      results.map { |r| MultiJson.decode(r) }
     end
     
-    def key(without_namespace, redis)
-      return without_namespace unless redis.respond_to? :namespace
-      "#{redis.namespace}:#{without_namespace}"
+    private
+    
+    # zinterstore command doesn't work with Redis::Namespace
+    # so we have to manually namespace the keys for this call only
+    def cache(key,words,redis)
+      interkeys = words.map { |word| namespaced("#{base}:#{word}", redis)  }
+      redis.zinterstore(namespaced(key,redis), interkeys)
+      redis.expire(key, 10 * 60) # expire after 10 minutes
+    end
+    
+    def namespaced(key, redis)
+      return key unless redis.respond_to? :namespace
+      "#{redis.namespace}:#{key}"
     end
     
 
